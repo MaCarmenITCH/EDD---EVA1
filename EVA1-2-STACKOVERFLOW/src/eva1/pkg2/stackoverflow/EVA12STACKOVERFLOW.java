0c2f412c1f11b0a00f33ca3ce57b1158a95068e3
MaCarmenITCH <104479276+MaCarmenITCH@users.noreply.github.com>
2022-08-19 09:53:02 -0600

    Add files via upload

---
+++ EVA1-2-STACKOVERFLOW/src/eva1/pkg2/stackoverflow/EVA12STACKOVERFLOW.java
@@ -0,0 +1,18 @@
+
+package eva1.pkg2.stackoverflow;
+
+public class EVA12STACKOVERFLOW {
+
+    public static void main(String[] args) {
+        A();
+        
+    }
+    public static void A(){
+        B();
+        
+    }
+    public static void B(){
+        A();
+        
+    }
+}
