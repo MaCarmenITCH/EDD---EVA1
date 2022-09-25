206192358dfe163b4e21a2cea150d35f77bb5409
MaCarmenITCH <104479276+MaCarmenITCH@users.noreply.github.com>
2022-09-24 20:09:59 -0600

    Add files via upload

---
+++ EVA1_14_PASO_REF/src/eva1_14_paso_ref/EVA1_14_PASO_REF.java
@@ -0,0 +1,25 @@
+
+package eva1_14_paso_ref;
+
+public class EVA1_14_PASO_REF {
+
+    public static void main(String[] args) {
+       
+        int[] arregloDatos = new int[100];
+        llenar(arregloDatos);
+        imprimir(arregloDatos);
+    }
+    public static void llenar(int[] arreglo){
+        for (int i = 0; i < arreglo.length; i++) {
+            arreglo[i] =100;
+            
+        }
+    }
+     public static void imprimir(int[] arreglo){
+        for (int i = 0; i < arreglo.length; i++) {
+            System.out.println("[" + arreglo[i] + "]");
+    
+}
+     
+     }
+}
