0e49da0588de72cd2f9f41f25a645aabed818aae
MaCarmenITCH <104479276+MaCarmenITCH@users.noreply.github.com>
2022-09-24 20:08:08 -0600

    Add files via upload
    A partir de aquí agrego los programas que me faltaron subir, tuve problema con mi cuenta de github y recién la recupero:')

--- EVA1_10_ARREGLO_CUBO/src/eva1_10_arreglo_cubo/EVA1_10_ARREGLO_CUBO.java
+++ EVA1_10_ARREGLO_CUBO/src/eva1_10_arreglo_cubo/EVA1_10_ARREGLO_CUBO.java
@@ -1,19 +1,32 @@
 
 package eva1_10_arreglo_cubo;
 
-
 public class EVA1_10_ARREGLO_CUBO {
 
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
         // TODO code application logic here
-        int [ ] [ ] [ ] cuboDatos = new int [ 3 ] [ 4 ] [ 5 ];
+        int[][][] cuboDatos =new int [3][4][5];
         for (int i = 0; i < cuboDatos.length; i++) {
-            for (int j = 0; j < cuboDatos[ i ].length; j++) {
-                for (int k = 0; k < cuboDatos[ i ][ j ].length; k++) {
-                    cuboDatos[ i ] [ j ] [ k ]  =  50; 
-                }           
-            }   
+            for (int j = 0; j < cuboDatos[i].length; j++) {
+                for (int k = 0; k < cuboDatos[i][j].length; k++) {
+                    cuboDatos[i][j][k] = 50;
+                    
+                }
+                
+            }
         }
-        System.out.println(" [ " + cuboDatos.length + " ] ");
-    }   
+        System.out.println("");
+        for (int i = 0; i < cuboDatos.length; i++) {
+            for (int j = 0; j < cuboDatos[i].length; j++) {
+                for (int k = 0; k < cuboDatos[i][j].length; k++) {
+                    System.out.print("[" + cuboDatos[i][j][k] + "]");
+                }
+                System.out.println("");      
+    }
+            System.out.println("");
+}
+    }
 }
